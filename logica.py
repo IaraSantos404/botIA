4b62747943297f45b70692606432f53ad7b8da14
Juan <danieljruan@gmail.com>
2026-09-15 12:33:03 -0300

    Logica e Verificação de bateria

---
+++ logica.py
@@ -0,0 +1,16 @@
+class BaseConhecimento:
+    def __init__(self):
+        self.regras = []
+    def adicionar_regra(self, condicao, acao):
+        self.regras.append((condicao, acao))
+    def interferir(self, sensores):
+        fatos_derivados = set()
+        for condicao, acao in self.regras:
+            if condicao(sensores):
+                fatos_derivados.add(acao)
+        return fatos_derivados
+def configurar_bc_robo():
+    bc = BaseConhecimento()
+    bc.adicionar_regra(lambda sensores: sensores['bateria_alta'], 'coleta_permitida')
+    bc.adicionar_regra(lambda sensores: sensores['carga_cheia'], 'precisa_descarregar')
+    bc.adicionar_regra(lambda bateria: bateria <= 30, 'precisa_recarregar')
